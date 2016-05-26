1dc61c5d2ca84a3fcec5f9df79ee7ffa5a649818
suxinde2009 <suxinde2009@126.com>
2016-05-26 09:49:39 +0800

    Update

---
+++ XDCommonLib/Utils/XDContinuousBootCrashPreventTool/AppDelegate+XDContinuousBootCrashPreventTool.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+XDContinuousBootCrashPreventTool.h
+//  XDContinuousBootCrashPreventTool
+//
+//  Created by suxinde on 16/5/25.
+//  Copyright © 2016年 suxinde. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (XDContinuousBootCrashPreventTool)
+
+@end
