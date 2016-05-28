c3ee938221ec5926be85a681138726b428c856b0
suxinde2009 <suxinde2009@126.com>
2016-05-28 23:14:52 +0800

    Update

--- XDCommonLib/Categories/CoreData/NSFetchRequest/NSFetchRequest+Extensions.h
+++ XDCommonLib/Categories/CoreData/NSFetchRequest/NSFetchRequest+Extensions.h
@@ -10,13 +10,23 @@
 typedef void (^FetchObjectsCallback)(NSArray *fetchedObjects, NSError *error);
 typedef void (^FetchObjectCallback)(id fetchedObject, NSError * error);
 
-@interface NSFetchRequest(Extensions)
+@interface NSFetchRequest (Extensions)
 
 + (id)fetchRequestWithEntity:(NSEntityDescription *)entity;
-+ (id)fetchRequestWithEntity:(NSEntityDescription *)entity predicate:(NSPredicate *)predicate;
-+ (id)fetchRequestWithEntity:(NSEntityDescription *)entity sortDescriptors:(NSArray *)sortDescriptors;
-+ (id)fetchRequestWithEntity:(NSEntityDescription *)entity predicate:(NSPredicate *)predicate sortDescriptors:(NSArray *)sortDescriptors;
 
-- (id)initWithEntity:(NSEntityDescription *)entity predicate:(NSPredicate *)predicate sortDescriptors:(NSArray *)sortDescriptors;
++ (id)fetchRequestWithEntity:(NSEntityDescription *)entity
+                   predicate:(NSPredicate *)predicate;
+
+
++ (id)fetchRequestWithEntity:(NSEntityDescription *)entity
+             sortDescriptors:(NSArray *)sortDescriptors;
+
++ (id)fetchRequestWithEntity:(NSEntityDescription *)entity
+                   predicate:(NSPredicate *)predicate
+             sortDescriptors:(NSArray *)sortDescriptors;
+
+- (id)initWithEntity:(NSEntityDescription *)entity
+           predicate:(NSPredicate *)predicate
+     sortDescriptors:(NSArray *)sortDescriptors;
 
 @end
