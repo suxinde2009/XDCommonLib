75286273cde414cce9e9103e1c342d732a505f31
suxinde2009 <suxinde2009@126.com>
2016-06-03 18:57:19 +0800

    Update

--- XDCommonLib/CommonBasicComponentLayer/Debug/UIView+DebugUtils.h
+++ XDCommonLib/CommonBasicComponentLayer/Debug/UIView+DebugUtils.h
@@ -8,8 +8,16 @@
 
 #import <UIKit/UIKit.h>
 
+/**
+ *  @brief UIView调试工具分类1
+ */
 @interface UIView (DebugUtils)
 
+/**
+ *  递归绘制该视图及其所有子视图的边框
+ *
+ *  @param b 是否递归绘制子视图的边框， NO则只绘制本身的边框
+ */
 - (void)drawBorderRecursive:(BOOL)b;
 
 @end
